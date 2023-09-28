93e82d807d09898daf30e06f32a187a9cf9eb461
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-09-28 21:22:50 +0330

    made the hero section responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,23 +3,24 @@ import logo from '../assets/logo.svg'
 
 const Hero = () => {
     return (
-        <div className='HeroSection w-full'>
-            <div className='header flex flex-row justify-between items-baseline w-full mt-4'>
-                <img src={logo} className='w-32' />
-                <button className='bg-black text-white rounded-full px-6 text-sm py-2'> Github </button>
-            </div>
+        <header className='HeroSection w-full'>
+            <nav className='header flex flex-row justify-between items-center md:items-baseline w-full mt-4'>
+                <img src={logo} className='w-28' alt='logo' />
+                <button className='bg-black text-white rounded-full px-6 text-sm py-2 duration-500 hover:bg-white hover:text-black hover:border hover:border-black'>
+                    Github </button>
+            </nav>
 
             <div className='Introduction mt-16'>
                 <div className='IntroductionTitle'>
-                    <h1 className='text-black text-6xl text-center font-extrabold'> Summarize Articles  with </h1>
-                    <h1 className='text-6xl text-center font-extrabold orange_gradient '> OpenAI GPT-4 </h1>
+                    <h1 className='text-black text-3xl sm:text-5xl md:text-6xl text-center font-extrabold'> Summarize Articles  with </h1>
+                    <h1 className='text-3xl sm:text-5xl md:text-6xl text-center font-extrabold orange_gradient '> OpenAI GPT-4 </h1>
                 </div>
 
-                <div className='IntroductionDescription text-center mt-5 text-[#4b5563] text-xl w-7/12 mx-auto'>
+                <div className='IntroductionDescription text-center mt-5 text-[#4b5563] text-xl w-full md:w-7/12 mx-auto'>
                     Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
                 </div>
             </div>
-        </div>
+        </header>
     )
 }
 
